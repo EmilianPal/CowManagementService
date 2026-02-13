c08af241228d3ab18f8e97f3ec1a3fd780283a24
EmilianPal <emilianpal21@gmail.com>
2026-02-13 18:09:11 +0200

    idk

---
+++ src-tauri/src/command/command_manager.rs
@@ -0,0 +1,50 @@
+use crate::command::command::Command;
+use rusqlite::Connection;
+
+
+pub struct CommandManager{
+    pub undo_stack: Vec<Box<dyn Command>>,
+    pub redo_stack: Vec<Box<dyn Command>>,
+}
+
+impl CommandManager {
+    pub fn new() -> Self {
+        Self {
+            undo_stack: Vec::new(),
+            redo_stack: Vec::new(),
+        }
+    }
+
+    pub fn execute(&mut self, mut command: Box<dyn Command>, conn: &mut Connection) -> Result<&mut dyn Command, String> { 
+        command.execute(conn)?;
+        self.undo_stack.push(command);
+        self.redo_stack.clear();
+        Ok(self.undo_stack.last_mut().unwrap().as_mut()) 
+    }
+
+    pub fn undo(&mut self, conn: &mut Connection) -> Result<(), String> {
+        if let Some(mut command) = self.undo_stack.pop() {
+            if let Err(e) = command.undo(conn) {
+                self.undo_stack.push(command);
+                return Err(e);
+            }
+
+            self.redo_stack.push(command);
+        }
+        Ok(())
+    }
+
+    pub fn redo(&mut self, conn: &mut Connection) -> Result<(), String> {
+        if let Some(mut command) = self.redo_stack.pop() {
+            if let Err(e) = command.execute(conn) {
+                self.redo_stack.push(command);
+                return Err(e);
+            }
+            self.undo_stack.push(command);
+        }
+        Ok(())
+    }
+}
+
+
+
--- src-tauri/src/command/mod.rs
+++ src-tauri/src/command/mod.rs
@@ -2,3 +2,4 @@ pub mod command;
 pub mod cow_command;
 pub mod birth_command;
 pub mod insemination_command;
+pub mod command_manager;
